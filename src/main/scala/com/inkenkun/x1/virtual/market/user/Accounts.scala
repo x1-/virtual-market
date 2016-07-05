cd6b186c2b3654c547968e4f509d8fe5e964ac57
x1- <viva008@gmail.com>
2016-07-05 19:19:12 +0900

    add holdings in account.

--- src/main/scala/com/inkenkun/x1/virtual/market/user/Accounts.scala
+++ src/main/scala/com/inkenkun/x1/virtual/market/user/Accounts.scala
@@ -1,12 +1,22 @@
 package com.inkenkun.x1.virtual.market.user
 
+import java.util.Date
 import com.inkenkun.x1.virtual.market.redis
 
 case class Account(
-  userId          : String,
-  userName        : String,
-  balance         : Option[BigDecimal],
-  marginRemaining : Option[BigDecimal]
+  userId          : String = "",
+  userName        : String = "",
+  balance         : Option[BigDecimal] = None,
+  marginRemaining : Option[BigDecimal] = None,
+  holdings        : List[Holding] = List.empty[Holding]
+)
+
+case class Holding (
+  time   : Date,
+  market : String,
+  code   : String,
+  price  : BigDecimal,
+  volume : Int
 )
 
 object Accounts{
@@ -17,4 +27,8 @@ object Accounts{
     val maybeUsers = redis.Handler.get( "users" )
     maybeUsers map( _.parseAs[List[Account]] ) getOrElse List.empty[Account]
   }
+
+  def retrieve( id: String ): Account =
+    initialUsers.find( _.userId == id ).getOrElse( Account() )
+
 }
